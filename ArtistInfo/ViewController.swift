3026fd85b5dd5b68c37febd1097b1388630319bd
Tonte Owuso <tonteowuso@gmail.com>
2024-02-11 10:07:51 +0000

    Initial Commit

---
+++ ArtistInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtistInfo
+//
+//  Created by Tonte Owuso on 11/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
